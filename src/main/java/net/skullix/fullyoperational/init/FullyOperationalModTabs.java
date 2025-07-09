394151acb52632a9042aa34d42d28aa1c8add310
Vermetra <natevitale10@gmail.com>
2025-07-09 15:13:28 -0400

    Updated Plush Tab

--- src/main/java/net/skullix/fullyoperational/init/FullyOperationalModTabs.java
+++ src/main/java/net/skullix/fullyoperational/init/FullyOperationalModTabs.java
@@ -9,7 +9,6 @@ import net.skullix.fullyoperational.FullyOperationalMod;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.network.chat.Component;
@@ -18,7 +17,7 @@ import net.minecraft.core.registries.Registries;
 public class FullyOperationalModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, FullyOperationalMod.MODID);
 	public static final RegistryObject<CreativeModeTab> FO_BLOCKS = REGISTRY.register("fo_blocks",
-			() -> CreativeModeTab.builder().title(Component.translatable("item_group.fully_operational.fo_blocks")).icon(() -> new ItemStack(Blocks.DRIPSTONE_BLOCK)).displayItems((parameters, tabData) -> {
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.fully_operational.fo_blocks")).icon(() -> new ItemStack(FullyOperationalModBlocks.VERMETRA_PLUSH.get())).displayItems((parameters, tabData) -> {
 				tabData.accept(FullyOperationalModBlocks.VERMETRA_PLUSH.get().asItem());
 				tabData.accept(FullyOperationalModBlocks.ZYPHRON_PLUSH.get().asItem());
 			}).build());
